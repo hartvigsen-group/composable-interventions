0d46e2b02da9845fc12b159d7c41c6d32a5a746f
ANURAG VAIDYA <ajv012@bucknell.edu>
2023-12-22 16:50:10 -0500

    added MQuAKE in edit_generator.py

--- utils/edit_generator.py
+++ utils/edit_generator.py
@@ -13,7 +13,7 @@ def get_edits(number_of_edits=3, file_path='data/counterfact/counterfact-edit.js
     # locality_prompt = []
     # locality_ground_truth = []
     locality_inputs = {
-    'common_key': {  'counterfact'
+    'common_key': {  'counterfact' 
         'prompt': [],
         'ground_truth': []
     }
@@ -39,3 +39,37 @@ def get_edits(number_of_edits=3, file_path='data/counterfact/counterfact-edit.js
         locality_inputs['common_key']['ground_truth'].append(entry['locality_ground_truth'])
 
     return prompts, ground_truth, target_new, subject, rephrase_prompt, locality_inputs
+
+def get_edits_mquake(number_of_edits=3, file_path='data/MQuAKE/MQuAKE-CF-3k.json'):
+    """
+    Create data folder with strcutr:
+    
+    data
+        --> MQuAKE
+            ---> MQuAKE-CF-3k.json
+            ---> MQuAKE-CF.json
+            ---> MQuAKE-CF-T.json
+    """
+    
+    with open(file_path, 'r') as file:
+        json_data = json.load(file)
+        
+    prompts = []
+    ground_truth = []
+    target_new = []
+    subject = []
+    accuracy_prompt = []
+    multiHop_prompts = []
+    multiHop_answers = []
+    
+    
+    for json_element in json_data[:number_of_edits]:
+        prompts.append(json_element['requested_rewrite'][0]['prompt'])
+        ground_truth.append(json_element['requested_rewrite'][0]['target_true'])
+        target_new.append(json_element['requested_rewrite'][0]['target_new'])
+        subject.append(json_element['requested_rewrite'][0]['subject'])
+        accuracy_prompt.append(json_element['requested_rewrite'][0]['question'])
+        multiHop_prompts.append(json_element['questions'])
+        multiHop_answers.append(json_element['new_answer'])
+    
+    return prompts, ground_truth, target_new, subject, accuracy_prompt, multiHop_prompts, multiHop_answers
