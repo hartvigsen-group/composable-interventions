b32e9af28c744cd286278d52692a2cc67a661da9
Kyle1668 <kyledevinobrien1@gmail.com>
2024-09-03 21:05:38 -0400

    Add bad vscode dbeug settings

--- unlearning_hparam_search.sh
+++ unlearning_hparam_search.sh
@@ -9,24 +9,46 @@ max_num_freeze_layers=$5
 learning_rates=(5e-6 1e-6 5e-5 1e-5 5e-4 1e-4 5e-3 1e-3)
 num_training_samples=(2000 1000 500 450 400 350 300 250 200 150 100 50 25 10)
 freeze_layers=($(seq $min_num_freeze_layers $max_num_freeze_layers))
-
-# print the number of combinations
-echo "Learning Rates: ${#learning_rates[@]}"
-echo "Training Sample Sizes: ${#num_training_samples[@]}"
-echo "Freeze Layers: ${#freeze_layers[@]}"
-
+num_runs=0
 
 if [ $unlearning_type == "ga" ]; then
-    num_runs=0
     for freeze_layer in "${freeze_layers[@]}"; do
         for lr in "${learning_rates[@]}"; do
             for train_sample_size in "${num_training_samples[@]}"; do
-                num_runs=$((num_runs+1))
+                num_runs=$((num_runs + 1))
                 echo run_exp.sh model_name=$model_name tag="ga_${model_tag}_hparam_search" unlearn=ga interventions=[unlearn] seed=42 wandb=online save_ckpt=False ga_train_size=$train_sample_size ga_lr=$lr
             done
         done
     done
-
-
-    echo "Number of combinations: ${num_runs}"
 fi
+
+retain_weights=(1 2 4 6 8 10 20 40 60 80 100)
+if [ $unlearning_type == "gd" ]; then
+    num_runs=0
+    for freeze_layer in "${freeze_layers[@]}"; do
+        for lr in "${learning_rates[@]}"; do
+            for train_sample_size in "${num_training_samples[@]}"; do
+                for retain_weight in "${retain_weights[@]}"; do
+                    num_runs=$((num_runs + 1))
+                    echo run_exp.sh model_name=$model_name tag="gd_${model_tag}_hparam_search" unlearn=gd interventions=[unlearn] seed=42 wandb=online save_ckpt=False ga_train_size=$train_sample_size ga_lr=$lr ga_retain_weight=$retain_weight
+                done
+            done
+        done
+    done
+fi
+
+alphas=(10000 1000 100 10 1)
+layers=(17 3)
+num_batches=(300 250 200 150 100)
+if [ $unlearning_type == "rmu" ]; then
+    for num_batch in "${num_batches[@]}"; do
+        for layer in "${layers[@]}"; do
+            for alpha in "${alphas[@]}"; do
+                num_runs=$((num_runs + 1))
+                echo run_exp.sh model_name=$model_name tag="rmu_${model_tag}_hparam_search" unlearn=rmu interventions=[unlearn] seed=42 wandb=online save_ckpt=False rmu_alpha=[$alpha,$alpha] rmu_layer_id=$layer rmu_max_num_batches=$num_batch
+            done
+        done
+    done
+fi
+
+echo "Number of combinations: ${num_runs}"
