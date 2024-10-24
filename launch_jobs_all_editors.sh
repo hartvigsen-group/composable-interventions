2dffcf38499baf79019793b7daaec3c98e2941fe
Arinbjorn Kolbeinsson <arinbjorn@gmail.com>
2024-10-24 04:13:31 -0400

    Minor changes

--- launch_jobs_all_editors.sh
+++ launch_jobs_all_editors.sh
@@ -8,53 +8,53 @@ sparsity_levels=(0.25 0.45 0.65)
 wbit_levels=(2 4 8)
 
 # Define different sets of configurations to be run
-# declare -a configs=(
-#     ### None ###
-#     "edit=none compression=none unlearn=none interventions=[] tag='None'"
-# )
+declare -a configs=(
+    ### None ###
+    "edit=none compression=none unlearn=none interventions=[] tag='None'"
+)
 
-# for editor in "${editors[@]}"; do
-#    ## Edit only ###
-#     configs+=("edit=${editor} compression=none unlearn=none interventions=[edit] tag='${editor}_Edit'")
-# done
+for editor in "${editors[@]}"; do
+   ## Edit only ###
+    configs+=("edit=${editor} compression=none unlearn=none interventions=[edit] tag='${editor}_Edit'")
+done
 
-# # Compress only - Wanda at different sparsity levels
-# for sparsity in "${sparsity_levels[@]}"; do
-#     configs+=("edit=none compression=wanda unlearn=none interventions=[compress] sparsity_ratio=${sparsity} tag='Compress_Wanda${sparsity}%'")
-# done
+# Compress only - Wanda at different sparsity levels
+for sparsity in "${sparsity_levels[@]}"; do
+    configs+=("edit=none compression=wanda unlearn=none interventions=[compress] sparsity_ratio=${sparsity} tag='Compress_Wanda${sparsity}%'")
+done
 
-# # Compress only - SparseGPT at different sparsity levels
-# for sparsity in "${sparsity_levels[@]}"; do
-#     configs+=("edit=none compression=sparsegpt unlearn=none interventions=[compress] sparsity_ratio=${sparsity} tag='Compress_SparseGPT${sparsity}%'")
-# done
+# Compress only - SparseGPT at different sparsity levels
+for sparsity in "${sparsity_levels[@]}"; do
+    configs+=("edit=none compression=sparsegpt unlearn=none interventions=[compress] sparsity_ratio=${sparsity} tag='Compress_SparseGPT${sparsity}%'")
+done
 
-# for editor in "${editors[@]}"; do
-#     # Edit then Compress - Wanda at different sparsity levels
-#     for sparsity in "${sparsity_levels[@]}"; do
-#         configs+=("edit=${editor} compression=wanda unlearn=none interventions=[edit,compress] sparsity_ratio=${sparsity} tag='${editor}-to-Wanda${sparsity}%'")
-#     done
-# done
+for editor in "${editors[@]}"; do
+    # Edit then Compress - Wanda at different sparsity levels
+    for sparsity in "${sparsity_levels[@]}"; do
+        configs+=("edit=${editor} compression=wanda unlearn=none interventions=[edit,compress] sparsity_ratio=${sparsity} tag='${editor}-to-Wanda${sparsity}%'")
+    done
+done
 
-# for editor in "${editors[@]}"; do
-#     # Edit then Compress - SparseGPT at different sparsity levels
-#     for sparsity in "${sparsity_levels[@]}"; do
-#         configs+=("edit=${editor} compression=sparsegpt unlearn=none interventions=[edit,compress] sparsity_ratio=${sparsity} tag='${editor}-to-SparseGPT${sparsity}%'")
-#     done
-# done
+for editor in "${editors[@]}"; do
+    # Edit then Compress - SparseGPT at different sparsity levels
+    for sparsity in "${sparsity_levels[@]}"; do
+        configs+=("edit=${editor} compression=sparsegpt unlearn=none interventions=[edit,compress] sparsity_ratio=${sparsity} tag='${editor}-to-SparseGPT${sparsity}%'")
+    done
+done
 
-# for editor in "${editors[@]}"; do
-#     # Compress with Wanda then Edit at different sparsity levels
-#     for sparsity in "${sparsity_levels[@]}"; do
-#         configs+=("edit=${editor} compression=wanda unlearn=none interventions=[compress,edit] sparsity_ratio=${sparsity} tag='Wanda${sparsity}%-to-${editor}'")
-#     done
-# done
+for editor in "${editors[@]}"; do
+    # Compress with Wanda then Edit at different sparsity levels
+    for sparsity in "${sparsity_levels[@]}"; do
+        configs+=("edit=${editor} compression=wanda unlearn=none interventions=[compress,edit] sparsity_ratio=${sparsity} tag='Wanda${sparsity}%-to-${editor}'")
+    done
+done
 
-# for editor in "${editors[@]}"; do
-#     # Compress with SparseGPT then Edit at different sparsity levels
-#     for sparsity in "${sparsity_levels[@]}"; do
-#         configs+=("edit=${editor} compression=sparsegpt unlearn=none interventions=[compress,edit] sparsity_ratio=${sparsity} tag='SparseGPT${sparsity}%-to-${editor}'")
-#     done
-# done
+for editor in "${editors[@]}"; do
+    # Compress with SparseGPT then Edit at different sparsity levels
+    for sparsity in "${sparsity_levels[@]}"; do
+        configs+=("edit=${editor} compression=sparsegpt unlearn=none interventions=[compress,edit] sparsity_ratio=${sparsity} tag='SparseGPT${sparsity}%-to-${editor}'")
+    done
+done
 
 # Compress only - AWQ at different wbit levels
 for wbit in "${wbit_levels[@]}"; do
@@ -97,6 +97,5 @@ done
 # Loop through each configuration and launch a job
 for cfg in "${configs[@]}"; do
     # Use sbatch for SLURM and passing Hydra config overrides
-    sbatch run_exp10x.sh $cfg
-    echo sbatch run_exp10x.sh $cfg
+    sbatch run_exp.sh $cfg
 done
